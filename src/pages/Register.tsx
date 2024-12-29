1dd9c65907e3b7b3ab7c5f4c96b948f3567c7200
SayyamButt <sayyambutt317@gmail.com>
2024-12-29 00:46:23 -0800

    add react Tanstack Query

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -12,63 +12,72 @@ import { Label } from "@radix-ui/react-label";
 
 const RegisterPage = () => {
   return (
-    <Card className="mx-auto max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-xl">Sign up</CardTitle>
-        <CardDescription>
-          Enter Your Information to Create a new Account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <div className="grid grid-cols-2 gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="first-name">First Name</Label>
-              <Input id="first-name" type="text" placeholder="Max" required />
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <Card className="mx-auto max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-xl">Sign up</CardTitle>
+            <CardDescription>
+              Enter Your Information to Create a new Account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-4">
+              <div className="grid grid-cols-2 gap-4">
+                <div className="grid gap-2">
+                  <Label htmlFor="first-name">First Name</Label>
+                  <Input
+                    id="first-name"
+                    type="text"
+                    placeholder="Max"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="last-name">Last Name</Label>
+                  <Input
+                    id="last-name"
+                    type="text"
+                    placeholder="Robinson"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="test@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="*******"
+                  required
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                Create an Account
+              </Button>
+              <Button variant="outline" className="w-full">
+                Sign up with Google
+              </Button>
             </div>
-            <div className="grid gap-2">
-              <Label htmlFor="last-name">Last Name</Label>
-              <Input
-                id="last-name"
-                type="text"
-                placeholder="Robinson"
-                required
-              />
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <Link to="/auth/login" className="underline">
+                Login
+              </Link>
             </div>
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="test@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              id="password"
-              type="password"
-              placeholder="*******"
-              required
-            />
-          </div>
-          <Button type="submit" className="w-full">
-            Create an Account
-          </Button>
-          <Button variant="outline" className="w-full">
-            Sign up with Google
-          </Button>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Already have an account?{" "}
-          <Link to="/login" className="underline">
-            Login
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   );
 };
 
